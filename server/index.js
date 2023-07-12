c4abab45c24f3d8a7b193cc7ea80c4590a2a5907
Aleksandro Milenkov <aleksandro.macedonia@gmail.com>
2023-07-12 17:36:21 +0200

    updating

--- server/index.js
+++ server/index.js
@@ -14,6 +14,9 @@ import postsRoutes from "./routes/postsRoutes.js";
 import { register } from "./controllers/auth.js";
 import { createPost } from "./controllers/posts.js";
 import { verifyToken } from "./middlewares/authMiddleware.js";
+import User from "./models/User.js";
+import Post from "./models/Post.js";
+import { users, posts } from "./data/index.js";
 
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
@@ -62,5 +65,8 @@ mongoose
   })
   .then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+    /* INSERT DATA ONLY ONCE */
+    // User.insertMany(users);
+    // Post.insertMany(posts);
   })
   .catch((error) => console.log(`${error} did not connected`));
